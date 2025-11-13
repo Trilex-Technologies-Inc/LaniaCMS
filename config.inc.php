ecb91dad783b8352e2724b69cae76c75d9fcf4b2
Marziye Momen <momen573@yahoo.com>
2025-11-13 19:19:23 +0330

    empty config

--- config.inc.php
+++ config.inc.php
@@ -1,37 +1,5 @@
 <?
 
     ## Config your database server
-    $dbname="mmomen";
-    $dbhost="localhost";
-    $dbuser="mmomen";
-    $dbpw="8baiSSwO0ixnydsizLN35hl4w";
-    $tablepre="tbl_ln_";
-    $dbtype="mysqli";
-
-    ## Site
-    $cfg_title="Lanai Core!";
-    $cfg_footer="&reg; Power by La-Nai Content Management System.<br/><a href=\"http://la-nai.sourceforge.net\" target=\"_blank\">La-Nai</a> is Free Software released under the <a href=\"license.txt\" title=\"GNU/GPL License\" target=\"_blank\">GNU/GPL license</a>.";
-    $cfg_url="http://mmomen.trilex.ca/laniazip";
-    $cfg_datadir="/home/mmomen/public_html/laniazip/datacenter";
-    $cfg_packagedir="/home/mmomen/public_html/laniazip/datacenter/package";
-    $cfg_dir="/home/mmomen/public_html/laniazip";
-    $cfg_email="admin@mmomen.trilex.ca";
-    $cfg_theme="vertex";
-
-    ## Misc
-    $cfg_off="no";
-    $cfg_offsettime=7;
-    $cfg_sendmail="yes";
-    $cfg_log="no";
-
-    ## smtp
-    $cfg_smtp_host="localhost";
-    $cfg_smtp_port="25";
-
-    ## Language
-    $cfg_lang="english";
-
-	## SEO
-	$cfg_seo="yes";
 
 ?>
