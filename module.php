221db818ef050c7b2ff919989f9a147655ee5beb
Marziye Momen <momen573@yahoo.com>
2025-11-19 21:44:39 +0330

    adding title to the content

--- module.php
+++ module.php
@@ -1,18 +1,19 @@
 <?php
 
-		include_once('setconfig.inc.php');
-		include_once('include/header.inc.php');
+include_once('setconfig.inc.php');
+include_once('include/header.inc.php');
 
-		$theme=New Theme();
-		$smarty->assign ("getLogoHeader", $theme->getLogoHeader());
-		$smarty->assign ("getFooter", $theme->getFooter());
-		$smarty->assign ("setBlockLeft", $theme->setBlock("l"));
-		$smarty->assign ("setBlockRight", $theme->setBlock("r"));
-		$smarty->assign ("setModule", $theme->getModule($_REQUEST['modname'],$_REQUEST['mf']));
-		//$smarty->assign ("setBlockCenter", $theme->setBlock("c"));
-		$smarty->assign ("setBlockTop", $theme->setBlock("t"));
-		$smarty->assign ("setBlockBottom", $theme->setBlock("b"));
-		$smarty->display('module.tpl');
+$theme = new Theme();
+$smarty->assign("getLogoHeader", $theme->getLogoHeader());
+$smarty->assign("getFooter", $theme->getFooter());
+$smarty->assign("setBlockLeft", $theme->setBlock("l"));
+$smarty->assign("setBlockRight", $theme->setBlock("r"));
+$smarty->assign("setModule", $theme->getModule($_REQUEST['modname'], $_REQUEST['mf']));
+$smarty->assign("nameModule", $_REQUEST['modname']);
+//$smarty->assign ("setBlockCenter", $theme->setBlock("c"));
+$smarty->assign("setBlockTop", $theme->setBlock("t"));
+$smarty->assign("setBlockBottom", $theme->setBlock("b"));
+$smarty->display('module.tpl');
 
-		include_once('include/footer.inc.php');
+include_once('include/footer.inc.php');
 ?>
--- modules/content/index.php
+++ modules/content/index.php
@@ -1,26 +1,69 @@
 <?
- 
-	if (!eregi("module.php", $_SERVER['PHP_SELF'])) {
-			die ("You can't access this file directly...");
-	}
-	
-	$module_name = basename(dirname(__FILE__));
-	$modfunction="modules/$module_name/module.php";
-	include_once($modfunction);
-	
-	$content = new Content();
-	/// settype
-	settype($_REQUEST['cid'],"integer");
-	$rs=$content->getContentById($_REQUEST['cid']);
-	if (($rs->recordcount())>0) {
-			
+
+if (!eregi("module.php", $_SERVER['PHP_SELF'])) {
+    die ("You can't access this file directly...");
+}
+
+$module_name = basename(dirname(__FILE__));
+$modfunction="modules/$module_name/module.php";
+include_once($modfunction);
+
+$content = new Content();
+/// settype
+settype($_REQUEST['cid'],"integer");
+$rs=$content->getContentById($_REQUEST['cid']);
+if (($rs->recordcount())>0) {
+
+    ?>
+    <?=$sys_lanai->setPageTitle($rs->fields['conTitle']); ?>
+
+
+    <?
+} else {
+    $sys_lanai->getErrorBox(_CONTENT_NOT_FOUND);
+}
 ?>
-<?=$sys_lanai->setPageTitle($rs->fields['conTitle']); ?>
-<span class="txtContentTitle"><?=$rs->fields['conTitle'] ?></span><br/><br/>
-<?=$rs->fields['conBody1']?>
-<?=$rs->fields['conBody2']?>
-<?
-	} else {
-		$sys_lanai->getErrorBox(_CONTENT_NOT_FOUND);
-	}
-?>
+<section class="article-hero">
+    <div class="container">
+        <h1 class="display-5 fw-bold"><?=$rs->fields['conTitle'] ?></h1>
+        <div class="article-meta mt-2">
+            <i class="fas fa-calendar-alt"></i> October 25, 2025 &nbsp;|&nbsp;
+            <i class="fas fa-user"></i> By Admin &nbsp;|&nbsp;
+            <i class="fas fa-folder"></i> Tech
+        </div>
+    </div>
+</section>
+
+<!-- Main Article Content -->
+<div class="container my-5">
+    <div class="row g-5">
+
+        <!-- Article -->
+        <div class="col-md-8">
+            <div class="article-content bg-white p-4 rounded shadow-sm">
+                <?=$rs->fields['conBody1']?>
+                <?=$rs->fields['conBody2']?>
+
+            </div>
+        </div>
+
+        <!-- Sidebar -->
+        <div class="col-md-4">
+            <div class="sidebar">
+                <h5 class="fw-bold mb-3">Related Articles</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-decoration-none">How AI is Changing the World</a></li>
+                    <li><a href="#" class="text-decoration-none">Top 10 Gadgets of 2025</a></li>
+                    <li><a href="#" class="text-decoration-none">The Rise of Quantum Computing</a></li>
+                </ul>
+
+                <hr>
+
+                <h5 class="fw-bold mb-3">Categories</h5>
+                <span class="badge bg-secondary me-1">Tech</span>
+                <span class="badge bg-secondary me-1">Science</span>
+                <span class="badge bg-secondary me-1">Innovation</span>
+            </div>
+        </div>
+    </div>
+</div>
